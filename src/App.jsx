0f295c675f373a2780af46b9d1bcb8875670266b
imadibrahimk <imadibrahim164@gmail.com>
2024-09-01 21:41:11 +0530

    testing code changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,170 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, redirect } from 'react-router-dom';
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Routes, redirect } from 'react-router-dom';
+// import LandingPage from './Pages/LandingPage';
+// import Explore from './Pages/Explore';
+// import LoginPage from './Pages/LoginPage';
+// import Navbar from './Components/Navbar';
+// import { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import TermsPage from './Pages/TermsPage';
+// import AboutUsPage from './Pages/AboutUsPage';
+// import Footer from './Components/Footer';
+// import RefundPolicy from './Pages/RefundPolicy';
+// import Payment from './Pages/Payment';
+// import PrivacyPolicy from './Pages/PrivacyPolicy';
+// import Modal from 'react-modal';
+// import ProfileModal from './Components/Modal/ProfileModal';
+// import Test from './Pages/Test';
+// import ProtectedRoute from './Components/ProtectedRoute';
+// import Dashboard from './Pages/Dashboard';
+
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import ErrorPage from './Components/ErrorPage';
+
+// Modal.setAppElement('#root');
+
+// function App() {
+//   const [user, setUser] = useState(null);
+//   const [loggedIn, setLoggedIn] = useState(false);
+//   const [modalOpen, setModalOpen] = useState(false);
+
+
+//   const params = new Proxy(new URLSearchParams(window.location.search), {
+//     get: (searchParams, prop) => searchParams.get(prop),
+//   });
+//   let value = params.token; 
+
+//   console.log('token getting from url :',value);
+
+//   if (value){
+//     localStorage.setItem('token', value);
+//     const apiUrl = import.meta.env.VITE_CLIENT_URL;
+//     window.location.href = apiUrl;
+//   }
+
+//   const storedToken = localStorage.getItem('token');
+// console.log('Token from local storage:', storedToken);
+  
+
+//   const getUser = async () => {
+//     try {
+//       const apiUrl = import.meta.env.VITE_API_URL;
+//       const url = `${apiUrl}login/success`;
+//       const headers = {
+//         Authorization: `Bearer ${storedToken}`,
+//       };
+//       const { data } = await axios.get(url, { withCredentials: true, headers: headers });
+      
+//       if (data.user) {
+//         console.log('====================================');
+//         console.log("user details :", data.user);
+//         console.log('====================================');
+//         console.log("user photo : ", data.photo);
+        
+//         setUser(data.user);
+//         setLoggedIn(true);
+//       } else {
+//         setLoggedIn(false);
+//       }
+//     } catch (error) {
+//       console.log("Login error: 121", error);
+//       setLoggedIn(false);
+//     }
+//   };
+
+//   useEffect(() => {
+//     getUser();
+//   }, []);
+
+//   const handleLogin = () => {
+//     console.log("Logged in");
+//   };
+
+//   const handleLogout = async () => {
+//     try {
+//       localStorage.clear();
+//       const apiUrl = import.meta.env.VITE_API_URL;
+//       await axios.get(`${apiUrl}/logout`, { withCredentials: true });
+//       setLoggedIn(false);
+//       setUser(null);
+//       console.log("Logged out");
+//       window.location.href = '/';
+//     } catch (error) {
+//       console.error('Error logging out:', error);
+//     }
+//   };
+
+//   const ToModalOpen = () => {
+//     setModalOpen(true);
+//     // console.log("opened");
+//   };
+
+//   const ToModalClose = () => {
+//     setModalOpen(false);
+//   };
+
+//   return (
+//     <Router>
+//       <Navbar
+//         loggedIn={loggedIn}
+//         handleLogout={handleLogout}
+//         user={user}
+//         ToModalOpen={ToModalOpen}
+//       />
+//       <Routes>
+//         <Route path='/' element={<LandingPage user={user} />} />
+//         {/* Redirect to "/" if logged in and tries to access "/login" */}
+//         <Route path='/login' element={
+//           <ProtectedRoute
+//             loggedIn={loggedIn}
+//           >
+//             <LoginPage handleLogin={handleLogin} getUser={getUser} />
+//           </ProtectedRoute>
+//         } />
+//          <Route path='/auth_callback' element={<AuthCallback />} />
+//         <Route path='/explore' element={<Explore user={user} />} />
+//         <Route path='/terms' element={<TermsPage />} />
+//         <Route path='/aboutus' element={<AboutUsPage />} />
+//         <Route path='/refund-policies' element={<RefundPolicy />} />
+//         <Route path='/privacy' element={<PrivacyPolicy />} />
+//         <Route path='/payment' element={<Payment />} />
+//         <Route path='/test' element={<Test />} />
+//         <Route path='/dashboard' element={ <Dashboard user={user} />}/>
+//         <Route path='*' element={ <ErrorPage/>}/>
+//       </Routes>
+      
+//         <Footer />
+
+//       <Modal
+//         isOpen={modalOpen}
+//         onRequestClose={ToModalClose}
+//         contentLabel="Profile"
+//         overlayClassName="fixed inset-0 bg-black bg-opacity-75"
+//         className="fixed inset-0 flex items-center justify-center"
+//       >
+//         <div className="bg-gray-200 rounded-2xl p-0 w-full max-w-md mx-auto">
+//           <ProfileModal
+//             ToModalClose={ToModalClose}
+//             handleLogout={handleLogout}
+//             user={user}
+//           />
+//         </div>
+//       </Modal>
+//       <ToastContainer />
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './Pages/LandingPage';
+import AuthCallback from './AuthCallback'; // Import the new component
 import Explore from './Pages/Explore';
 import LoginPage from './Pages/LoginPage';
 import Navbar from './Components/Navbar';
-import { useState, useEffect } from 'react';
 import axios from 'axios';
 import TermsPage from './Pages/TermsPage';
 import AboutUsPage from './Pages/AboutUsPage';
@@ -17,8 +177,7 @@ import ProfileModal from './Components/Modal/ProfileModal';
 import Test from './Pages/Test';
 import ProtectedRoute from './Components/ProtectedRoute';
 import Dashboard from './Pages/Dashboard';
-
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ErrorPage from './Components/ErrorPage';
 
@@ -29,23 +188,8 @@ function App() {
   const [loggedIn, setLoggedIn] = useState(false);
   const [modalOpen, setModalOpen] = useState(false);
 
-
-  const params = new Proxy(new URLSearchParams(window.location.search), {
-    get: (searchParams, prop) => searchParams.get(prop),
-  });
-  let value = params.token; 
-
-  console.log('token getting from url :',value);
-
-  if (value){
-    localStorage.setItem('token', value);
-    const apiUrl = import.meta.env.VITE_CLIENT_URL;
-    window.location.href = apiUrl;
-  }
-
   const storedToken = localStorage.getItem('token');
-console.log('Token from local storage:', storedToken);
-  
+  console.log('Token from local storage:', storedToken);
 
   const getUser = async () => {
     try {
@@ -57,18 +201,14 @@ console.log('Token from local storage:', storedToken);
       const { data } = await axios.get(url, { withCredentials: true, headers: headers });
       
       if (data.user) {
-        console.log('====================================');
-        console.log("user details :", data.user);
-        console.log('====================================');
-        console.log("user photo : ", data.photo);
-        
+        console.log("User details:", data.user);
         setUser(data.user);
         setLoggedIn(true);
       } else {
         setLoggedIn(false);
       }
     } catch (error) {
-      console.log("Login error: 121", error);
+      console.log("Login error:", error);
       setLoggedIn(false);
     }
   };
@@ -97,7 +237,6 @@ console.log('Token from local storage:', storedToken);
 
   const ToModalOpen = () => {
     setModalOpen(true);
-    // console.log("opened");
   };
 
   const ToModalClose = () => {
@@ -114,11 +253,9 @@ console.log('Token from local storage:', storedToken);
       />
       <Routes>
         <Route path='/' element={<LandingPage user={user} />} />
-        {/* Redirect to "/" if logged in and tries to access "/login" */}
+        <Route path='/auth_callback' element={<AuthCallback />} /> {/* Updated Route for AuthCallback */}
         <Route path='/login' element={
-          <ProtectedRoute
-            loggedIn={loggedIn}
-          >
+          <ProtectedRoute loggedIn={loggedIn}>
             <LoginPage handleLogin={handleLogin} getUser={getUser} />
           </ProtectedRoute>
         } />
@@ -129,11 +266,11 @@ console.log('Token from local storage:', storedToken);
         <Route path='/privacy' element={<PrivacyPolicy />} />
         <Route path='/payment' element={<Payment />} />
         <Route path='/test' element={<Test />} />
-        <Route path='/dashboard' element={ <Dashboard user={user} />}/>
-        {/* <Route path='*' element={ <ErrorPage/>}/> */}
+        <Route path='/dashboard' element={<Dashboard user={user} />} />
+        <Route path='*' element={<ErrorPage />} />
       </Routes>
       
-        <Footer />
+      <Footer />
 
       <Modal
         isOpen={modalOpen}
